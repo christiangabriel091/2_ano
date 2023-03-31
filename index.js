a5ca17329b87823a2fe95a963561887a16db2e6b
christiangabriel091 <129456288+christiangabriel091@users.noreply.github.com>
2023-03-30 21:35:39 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,117 @@
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+const img3 = document.getElementById("img3");
+const img4 = document.getElementById("img4");
+const img5 = document.getElementById("img5");
+const img6 = document.getElementById("img6");
+const img7 = document.getElementById("img7");
+const img8 = document.getElementById("img8");
+const img9 = document.getElementById("img9");
+
+const clickImg1 = () => {
+    img1.style.display = "block";
+    img2.style.display = "none";
+    img3.style.display = "none";
+    img4.style.display = "none";
+    img5.style.display = "none";
+    img6.style.display = "none";
+    img7.style.display = "none";
+    img8.style.display = "none";
+    img9.style.display = "none";
+}
+
+const clickImg2 = () => {
+    img1.style.display = "none";
+    img2.style.display = "block";
+    img3.style.display = "none";
+    img4.style.display = "none";
+    img5.style.display = "none";
+    img6.style.display = "none";
+    img7.style.display = "none";
+    img8.style.display = "none";
+    img9.style.display = "none";
+}
+
+const clickImg3 = () => {
+    img1.style.display = "none";
+    img2.style.display = "none";
+    img3.style.display = "block";
+    img4.style.display = "none";
+    img5.style.display = "none";
+    img6.style.display = "none";
+    img7.style.display = "none";
+    img8.style.display = "none";
+    img9.style.display = "none";
+}
+
+const clickImg4 = () => {
+    img1.style.display = "none";
+    img2.style.display = "none";
+    img3.style.display = "none";
+    img4.style.display = "block";
+    img5.style.display = "none";
+    img6.style.display = "none";
+    img7.style.display = "none";
+    img8.style.display = "none";
+    img9.style.display = "none";
+}
+
+const clickImg5 = () => {
+    img1.style.display = "none";
+    img2.style.display = "none";
+    img3.style.display = "none";
+    img4.style.display = "none";
+    img5.style.display = "block";
+    img6.style.display = "none";
+    img7.style.display = "none";
+    img8.style.display = "none";
+    img9.style.display = "none";
+}
+
+const clickImg6 = () => {
+    img1.style.display = "none";
+    img2.style.display = "none";
+    img3.style.display = "none";
+    img4.style.display = "none";
+    img5.style.display = "none";
+    img6.style.display = "block";
+    img7.style.display = "none";
+    img8.style.display = "none";
+    img9.style.display = "none";
+}
+
+const clickImg7 = () => {
+    img1.style.display = "none";
+    img2.style.display = "none";
+    img3.style.display = "none";
+    img4.style.display = "none";
+    img5.style.display = "none";
+    img6.style.display = "none";
+    img7.style.display = "block";
+    img8.style.display = "none";
+    img9.style.display = "none";
+}
+
+const clickImg8 = () => {
+    img1.style.display = "none";
+    img2.style.display = "none";
+    img3.style.display = "none";
+    img4.style.display = "none";
+    img5.style.display = "none";
+    img6.style.display = "none";
+    img7.style.display = "none";
+    img8.style.display = "block";
+    img9.style.display = "none";
+}
+
+const clickImg9 = () => {
+    img1.style.display = "none";
+    img2.style.display = "none";
+    img3.style.display = "none";
+    img4.style.display = "none";
+    img5.style.display = "none";
+    img6.style.display = "none";
+    img7.style.display = "none";
+    img8.style.display = "none";
+    img9.style.display = "block";
+}
